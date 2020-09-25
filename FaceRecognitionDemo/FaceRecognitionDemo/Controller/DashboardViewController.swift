b2be39dc0d4668d9b34db9b02d0b05a70a08cd68
Satabhisha Roy <satabhisha.innits@gmail.com>
2020-09-25 17:30:14 +0530

    initial commit

---
+++ FaceRecognitionDemo/FaceRecognitionDemo/Controller/DashboardViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DashboardViewController.swift
+//  FaceRecognitionDemo
+//
+//  Created by SATABHISHA ROY on 23/09/20.
+//  Copyright © 2020 SATABHISHA ROY. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btn_selfie(_ sender: Any) {
+        self.performSegue(withIdentifier: "selfie", sender: nil)
+    }
+    
+
+    @IBAction func btn_realtime(_ sender: Any) {
+        self.performSegue(withIdentifier: "realtimedetector", sender: nil)
+    }
+}
